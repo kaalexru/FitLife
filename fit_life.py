f66c83c47ebd195db0305716d385ccfbac550b30
Konstantin Aleksandrov <kaalex.ru@yandex.ru>
2026-05-27 23:23:14 +0300

    Исправлена ошибка вывода строки

--- fit_life.py
+++ fit_life.py
@@ -79,10 +79,9 @@ water_l = round(water_ml / ML_PER_LITER, 1)
 
 # Вывод отчета
 print("\n")
-print(
-    f"Отчет для пользователя: {user_name}, "
-    f"{user_age} {get_age_word(user_age)}\n"
-)
+
+print(f"Отчет для пользователя: {user_name}, ", end="")
+print(f"{user_age} {get_age_word(user_age)}\n")
 
 print(f"Ваш индекс массы тела: {bmi:.1f}")
 print(f"Рекомендуемая норма воды: {water_l:.1f} л в день\n")
